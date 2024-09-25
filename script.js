c1934e854f8106a744167e67c38bdd3ae0045310
andrechamelete <chamelete@gmail.com>
2024-09-25 12:02:49 -0300

    teste sem extensão de arquivo

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.language').addEventListener('mouseenter', function() {
-    var tooltip = document.querySelector('.tooltip');
-    tooltip.style.display = 'block';
-});
-
-document.querySelector('.language').addEventListener('mouseout', function() {
-    var tooltip = document.querySelector('.tooltip');
-    tooltip.style.display = 'none';
-});
