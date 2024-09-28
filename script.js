a5ec4750c6fe731eb5ba9729d146dd41eef13bba
andrechamelete <chamelete@gmail.com>
2024-09-28 13:20:39 -0300

    responsive design

--- script.js
+++ script.js
@@ -11,3 +11,13 @@ document.addEventListener("DOMContentLoaded", function() {
     const left = document.querySelectorAll('.impar');
     left.forEach(el => observer.observe(el));
 });
+
+// Seleciona o ícone de menu e o próprio menu
+const menuToggle = document.querySelector('.menu-toggle');
+const listaMenu = document.querySelector('.lista-menu');
+
+// Adiciona o evento de clique
+menuToggle.addEventListener('click', function() {
+    // Alterna a classe 'active' para mostrar ou esconder o menu
+    listaMenu.classList.toggle('active');
+});
